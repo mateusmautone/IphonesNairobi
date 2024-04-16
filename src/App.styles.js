b9c96c209797db661e94ba9e34222094dda1a2fa
mateusmautone <mateusmanico@gmail.com>
2024-04-16 19:43:44 -0300

    tela login crua + sistema login

--- src/App.styles.js
+++ src/App.styles.js
@@ -3,13 +3,30 @@ import styled from "styled-components";
 export const HeaderLogin = styled.header`
     background-color: black;
     display: flex;
-    justify-content: center;
-    align-items: center; 
-    height: 100%; -
+    justify-content: space-between; /* Alinha os itens à esquerda e à direita */
+    align-items: center; /* Alinha os itens verticalmente no centro */
+    height: 100%;
 `;
 
 export const Title = styled.h1`
     color: white;
-    padding: 15px;
-    
+    padding-left: 100px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+`;
+
+export const FormsLogin = styled.div``;
+
+export const Subtitle = styled.div`
+    color: white;
+    display: flex; 
+    align-items: center; 
+    gap: 20px; 
+    padding-right: 100px;
+    font-size: 20px;
+`;
+
+export const Icon = styled.i`
+    color: white;
+    font-size: 2.0rem;
 `;
--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,44 @@
 import React from "react";
 import * as S from './App.styles'
+import 'primeicons/primeicons.css';
+import { useState } from "react";
 
 const Login = () => {
+
+    const[nome, setNome] = useState('');
+    const[senha, setSenha] = useState('');
+
+    const handleChangeSenha = (event) =>{
+        setSenha(event.target.value);
+        
+    }
+
+    const handleChangeNome = (event) =>{
+        setNome(event.target.value);
+        
+    }
+
+    const handleClick = () => {
+        console.log(senha + nome)
+    }
+
     return(
-        <S.HeaderLogin>
-            <S.Title>Iphones do Nairobi</S.Title>
-        </S.HeaderLogin>
+        <div className="container">
+                <S.HeaderLogin>
+                    <S.Title>Iphones do Nairobi</S.Title>
+                    <S.Subtitle>
+                        <i className="pi pi-lock" style={{ color: 'white',  fontSize: '2.0rem'  }}></i>
+                        100% Seguro
+                    </S.Subtitle>
+                </S.HeaderLogin>
+            <S.FormsLogin>    
+                <i className="pi pi-envelope"></i>           
+                <input type="text" value={nome} onChange={handleChangeNome} placeholder="Digite seu e-mail"></input>
+                <i className="pi pi-key"></i>
+                <input type="password" value={senha} onChange={handleChangeSenha} id="senha" placeholder="Digite sua senha"></input>
+                <button onClick={handleClick}>Entrar</button>
+            </S.FormsLogin>
+        </div>
     )
 }
 
